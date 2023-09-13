5f36965e7d951c3ec4b7fd9040a6dd2119e447ef
Manish Sajandasani <msajandasani@gmail.com>
2023-09-13 23:03:25 +0530

    Generated a component from scratch; component decorator; selector, template, templateUrl, styles, styleUrls; component registration;

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+
+  // template: `<h1>
+  //   This is Navbar 
+  //   <span>Component</span>
+  // </h1>`,
+  
+  // styles: [`h1 {
+  //           color: blue
+  //         }`, `h1 span {
+  //           color: orange
+  //         }`]
+
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css'],
+})
+
+export class NavbarComponent {
+
+}
